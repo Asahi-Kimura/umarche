5a48a93ff8cb5fbc8b1ae1d4253083855fb025e8
Asahi Kimura <kimura.juswork2nd@gmail.com>
2022-09-29 15:51:19 +0900

    mergeについて

--- resources/views/components/tests/card.blade.php
+++ resources/views/components/tests/card.blade.php
@@ -1,4 +1,12 @@
-<div class="border-2 shadow-md w-1/4 p-2">
+@props([
+    'title'=>'初期値です',
+    'message'=>'初期値です',
+    'content'=>'初期値です'
+])
+
+<div {{$attributes->merge([
+    'class'=>'border-2 shadow-md w-1/4 p-2'
+]) }} >
     <div>{{$title}}</div>
     <div>画像</div>
     <div>{{$content}}</div>
--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -3,7 +3,8 @@
     コンポーネント１
 
     <x-tests.card title='タイトル1' content='本文' :message='$message' />
-    
+    <x-tests.card title='タイトル２' content='本文' :message='$message' />
+    <x-tests.card title='cssを変更したい' class='bg-red-300' content='本文' :message='$message' />
     
 
 </x-tests.app>
